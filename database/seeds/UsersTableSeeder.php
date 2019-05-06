715b6bf418aa77b97281cb32f3913122fbe36384
Martin Laiz <martinlaizg@gmail.com>
2019-05-06 18:40:38 +0200

    Añadida contraseña hasheada

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Martin';
         $user->email = 'martinlaizg@gmail.com';
         $user->username = 'martinlaizg';
-        $user->password = 'martinlaizg';
+        $user->password = 'cd17168a0bb539e433ad3d527067defd5f5c1588f12fdf420da330d17a303b3e8ebaac6ea1729a4e4089c51517b6b0ef6899ea32b1eaf5d998b7e86ab8b4e8c9';
         $user->user_type = 'admin';
         $user->image = 'https://avatars1.githubusercontent.com/u/16946027?s=460&v=4';
         $user->save();
